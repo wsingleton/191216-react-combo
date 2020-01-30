8129d5b84d4325965fafffec3dbf5941f93ec72e
Abatson <abatson94@gmail.com>
2020-01-30 11:18:02 -0500

    added redux version

---
+++ react/sad-name-ui-redux/src/components/chuck-norris-component/ChuckNorrisComponent.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { getChuckNorrisJoke } from '../../remote/chuck-norris-clients/chuck-norris-jokes';
+
+interface IChuckNorrisState {
+    currentJoke: string
+    numJokesAcquired: number
+}
+
+export class ChuckNorrisComponent extends React.Component<any, IChuckNorrisState>{
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            currentJoke: 'This is a very funny Chuck Norris joke',
+            numJokesAcquired: 0
+        }
+    }
+
+
+
+    async componentDidMount() {
+        let joke = await getChuckNorrisJoke()
+        this.setState({
+            ...this.state,
+            currentJoke: joke,
+            numJokesAcquired: this.state.numJokesAcquired + 1
+
+        })
+    }
+
+    getNewJoke = async () =>{
+        let joke = await getChuckNorrisJoke()
+        this.setState({
+            ...this.state,
+            currentJoke: joke,
+            numJokesAcquired: this.state.numJokesAcquired + 1
+
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <p>This is the ChuckNorris Joke Component</p>
+                <p> You have gotten this many jokes: {this.state.numJokesAcquired}</p>
+                <p>Current Joke</p>
+                <p>{this.state.currentJoke}</p>
+                <button onClick={this.getNewJoke}>GetNewJoke</button>
+            </div>
+        )
+    }
+}
