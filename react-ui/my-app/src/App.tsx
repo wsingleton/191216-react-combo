2147b3b3debb0fb1de4c6a781d85540c5cab8948
Ben <benjaminstevenburke@gmail.com>
2020-02-04 11:06:41 -0500

    hmmmm

--- react-ui/my-app/src/App.tsx
+++ react-ui/my-app/src/App.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { FirstComponent } from './components/first-component/FirstComponent';
-import { SecondComponent } from './components/second-component/SecondComponent';
+import { SecondComponent } from './components/second-component/SecondComponent'
 import { PropComponent } from './components/prop-component/PropComponent';
 import { ClickerComponent } from './components/click-component/ClickerComponent';
-import { BrowswerRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <FirstComponent/>
-      <SecondComponent/>
-      <ClickerComponent/>
-      <PropComponent name={'Ben'} animal={'dog'}/>
+    <Router>
+      <Switch>
+      <Route path='/' component={SecondComponent}/>
+      <Route path='/clicker' component={ClickerComponent}/>
+      <Route path='/props' render={() => <PropComponent name={'Alec'} animal={'grey'} />} />
+        <Route path='/' component={FirstComponent}/>
+      </Switch>
+    </Router>
+      
     </div>
   );
 }
